75e9a8a4d42a5be1520b4d3a96b99f10167be562
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-03-27 13:00:10 +0500

    CopyRight Section

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,12 +4,20 @@
       <div class="row">
         <div class="col-md-3 col-sm-6">
           <div class="footer_box">
-            <img src="/assets/images/footer_logo.png" alt="Footer Logo">
+            <img src="/assets/images/footer_logo.png" alt="Footer Logo" />
             <p>855 Road Broklyn Street, 600 New York, USA</p>
-            <span class="support_email"><i class="fa fa-envelope"></i> <a href="mailto:layerdrops@gmail.com">needhelp@refix.com</a></span>
-            <span class="support_num"><i class="fa fa-phone-square"></i> <a href="">666 888 0000</a></span>
-          </div><!--end .footer_box-->
-        </div><!--end .col-md-3-->
+            <span class="support_email"
+              ><i class="fa fa-envelope"></i>
+              <a href="mailto:layerdrops@gmail.com">needhelp@refix.com</a></span
+            >
+            <span class="support_num"
+              ><i class="fa fa-phone-square"></i>
+              <a href="">666 888 0000</a></span
+            >
+          </div>
+          <!--end .footer_box-->
+        </div>
+        <!--end .col-md-3-->
         <div class="col-md-3 col-sm-6">
           <div class="footer_box service">
             <h3>Services</h3>
@@ -19,16 +27,16 @@
               <li><a href="#">Refrigerator</a></li>
               <li><a href="#">Bar Cooler</a></li>
             </ul>
-          </div><!--end .footer_box-->
-        </div><!--end .col-md-3-->
+          </div>
+          <!--end .footer_box-->
+        </div>
+        <!--end .col-md-3-->
         <div class="col-md-3 col-sm-6">
           <div class="footer_box latest_posts">
             <h3>Latest Posts</h3>
             <ul>
               <li>
-                <a href="#">
-                  A Clean Website Gives More Experience
-                </a>
+                <a href="#"> A Clean Website Gives More Experience </a>
                 <span class="date">Jul 12 - 2019</span>
               </li>
               <li>
@@ -38,8 +46,10 @@
                 <span class="date">Jul 12 - 2019</span>
               </li>
             </ul>
-          </div><!--end .footer_box-->
-        </div><!--end .col-md-3-->
+          </div>
+          <!--end .footer_box-->
+        </div>
+        <!--end .col-md-3-->
         <div class="col-md-3 col-sm-6">
           <div class="footer_box instagram">
             <h3>Instagram</h3>
@@ -63,10 +73,14 @@
                 <a href="#" class="inst_bg bg_6"></a>
               </li>
             </ul>
-          </div><!--end .footer_box-->
-        </div><!--end .col-md-3-->
-      </div><!--end .row-->
-    </div><!--end .container-->
+          </div>
+          <!--end .footer_box-->
+        </div>
+        <!--end .col-md-3-->
+      </div>
+      <!--end .row-->
+    </div>
+    <!--end .container-->
 
     <!-- copyright section -->
     <div class="copyright_area">
@@ -75,37 +89,60 @@
           <div class="col-md-6 col-sm-12">
             <div class="copyright_left">
               <ul class="social_media">
-                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                <li><a href="#"><i class="fa fa-youtube-play"></i></a></li>
+                <li>
+                  <a href="#"><i class="fa fa-twitter"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-facebook"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-linkedin"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-youtube-play"></i></a>
+                </li>
               </ul>
-              <p>&copy; copyright 2020 by Layerdrops.com</p>
-            </div><!--end .copyright_left-->
-          </div><!--end .col-md-6-->
+              <p>
+                &copy; copyright {{ getCurrentYear() }} - FK Air Cond Services.
+              </p>
+            </div>
+            <!--end .copyright_left-->
+          </div>
+          <!--end .col-md-6-->
           <div class="col-md-6 col-sm-12">
             <div class="copyright_right">
               <ul class="menu">
-                <li><a href="#">About</a></li>
-                <li><a href="#">Book Online</a></li>
-                <li><a href="#">Testimonials</a></li>
-                <li><a href="#">Gallery</a></li>
-                <li><a href="#">Contact</a></li>
+                <li>
+                  <a href="https://wahabsharif.me/" target="_blank"
+                    >Design and Developed with Passion by
+                    <img
+                      src="/assets/images/wahab-sharif-logo-4-neon.svg"
+                      alt="Developer Logo"
+                  /></a>
+                </li>
               </ul>
-            </div><!--end .copyright_right-->
-          </div><!--end .col-md-6-->
-        </div><!--end .row-->
-      </div><!--end .container-->
-    </div><!--end .copyright_area-->
+            </div>
+            <!--end .copyright_right-->
+          </div>
+          <!--end .col-md-6-->
+        </div>
+        <!--end .row-->
+      </div>
+      <!--end .container-->
+    </div>
+    <!--end .copyright_area-->
   </footer>
 </template>
 
 <script>
-    export default {
-      name: "Footer"
-    }
+export default {
+  name: "Footer",
+  methods: {
+    getCurrentYear() {
+      return new Date().getFullYear();
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
