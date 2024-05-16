ca06376314279449bd1a917cf57c865d1d6b5238
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-05-17 00:03:37 +0500

    contact page

--- components/Contact.vue
+++ components/Contact.vue
@@ -37,42 +37,51 @@
       </div>
       <div class="container">
         <div class="row">
-          <div class="hero-section-title">
-            <i class="icon-snowflake"></i>
-            <h4>CONTACT US</h4>
-            <h1>Get in touch with us</h1>
-          </div>
           <div class="col-lg-6 col-md-6">
-            <div class="contact-details">
-              <form action="#" method="post">
-                <input
-                  type="text"
-                  name="name"
-                  class="input_mr gray_bg"
-                  placeholder="Full name"
-                />
-                <input
-                  type="EMAIL"
-                  name="email"
-                  class="gray_bg"
-                  placeholder="Your email"
-                />
-                <textarea
-                  class="gray_bg"
-                  name="content"
-                  id="textarea"
-                  cols="30"
-                  rows="10"
-                  placeholder="Write message"
-                ></textarea>
-                <button class="btn-orange" type="submit" value="Request a Quote Now">
-                  Send Message Now
-                </button>
-              </form>
+            <div class="formbold-main-wrapper">
+              <div class="formbold-form-wrapper">
+                <form action="#" method="POST">
+                  <div class="formbold-mb-5">
+                    <label for="email" class="formbold-form-label">Email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      placeholder="Enter your Email Address"
+                      class="formbold-form-input"
+                    />
+                  </div>
+                  <div class="formbold-mb-5">
+                    <label for="name" class="formbold-form-label">Name</label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      placeholder="Enter Your Name"
+                      class="formbold-form-input"
+                    />
+                  </div>
+                  <div class="formbold-mb-5">
+                    <label for="phone" class="formbold-form-label">Message</label>
+                    <input
+                      type="text"
+                      name="phone"
+                      id="phone"
+                      placeholder="Enter your phone Message"
+                      class="formbold-form-input"
+                    />
+                  </div>
+                  <div>
+                    <button class="formbold-btn">Submit</button>
+                  </div>
+                </form>
+              </div>
             </div>
+
             <!--end .contact-details-->
           </div>
           <div class="col-lg-6 col-md-6">
+            <h1 class="text-start">Get in touch with us</h1>
             <div class="contact-details">
               <p class="text-justify">
                 "FK Aircond offers comprehensive HVAC solutions tailored to your needs.
@@ -121,6 +130,95 @@ export default {
 </script>
 
 <style scoped>
+.formbold-mb-5 {
+  margin-bottom: 20px;
+}
+.formbold-pt-3 {
+  padding-top: 12px;
+}
+.formbold-main-wrapper {
+  margin-top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 48px;
+}
+
+.formbold-form-wrapper {
+  margin: 0 auto;
+  max-width: 550px;
+  width: 100%;
+  background: white;
+}
+.formbold-form-label {
+  text-align: left;
+  display: block;
+  font-weight: 500;
+  font-size: 16px;
+  color: #07074d;
+  margin-bottom: 12px;
+}
+.formbold-form-label-2 {
+  font-weight: 600;
+  font-size: 20px;
+  margin-bottom: 20px;
+}
+
+.formbold-form-input {
+  width: 100%;
+  padding: 12px 24px;
+  background: white;
+  font-weight: 500;
+  font-size: 16px;
+  color: #6b7280;
+  border: none;
+  border-bottom: 2px solid black;
+}
+.formbold-form-input:focus {
+  border-color: #b8272e;
+  box-shadow: 0px 3px 8px rgba(0, 0, 0, 0.05);
+}
+
+.formbold-btn {
+  text-align: center;
+  font-size: 16px;
+  border-radius: 50px;
+  padding: 14px 32px;
+  border: none;
+  font-weight: 600;
+  background-color: #b8272e;
+  color: white;
+  width: 100%;
+  cursor: pointer;
+}
+.formbold-btn:hover {
+  background-color: #225685;
+  box-shadow: 0px 3px 8px rgba(0, 0, 0, 0.05);
+}
+
+.formbold--mx-3 {
+  margin-left: -12px;
+  margin-right: -12px;
+}
+.formbold-px-3 {
+  padding-left: 12px;
+  padding-right: 12px;
+}
+.flex {
+  display: flex;
+}
+.flex-wrap {
+  flex-wrap: wrap;
+}
+.w-full {
+  width: 100%;
+}
+@media (min-width: 540px) {
+  .sm\:w-half {
+    width: 50%;
+  }
+}
+
 .contact-social .social_media li {
   display: inline-block;
   margin-top: 10px;
